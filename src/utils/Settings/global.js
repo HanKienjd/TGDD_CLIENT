de59f0637420df521b3c01fbfb0957d6d4d4f461
Hankienjd <kienp544@gmail.com>
2022-08-04 23:19:01 +0700

    deloy app

---
+++ src/utils/Settings/global.js
@@ -0,0 +1,3 @@
+export const TOKEN = "TOKEN_THE_GIOI_DI_DONG";
+export const DOMAIN = "http://localhost:5000/api/v1"; // NÀY LÀ API CỦA MÌNH NÊN MÌNH CŨNG K SHARE ĐƯỢC
+// LOCAL ----> http://localhost:5000
