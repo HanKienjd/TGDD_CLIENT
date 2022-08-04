2f6b85ff3ae34a2709e868db2375637b8d121530
Hankienjd <kienp544@gmail.com>
2022-08-04 23:44:33 +0700

    deloy app

--- src/utils/Settings/global.js
+++ src/utils/Settings/global.js
@@ -1,3 +1,3 @@
 export const TOKEN = "TOKEN_THE_GIOI_DI_DONG";
-export const DOMAIN = "http://localhost:5000/api/v1"; // NÀY LÀ API CỦA MÌNH NÊN MÌNH CŨNG K SHARE ĐƯỢC
+export const DOMAIN = "https://arcane-spire-14916.herokuapp.com/api/v1"; // NÀY LÀ API CỦA MÌNH NÊN MÌNH CŨNG K SHARE ĐƯỢC
 // LOCAL ----> http://localhost:5000
